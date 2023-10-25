ce5eac814ad1ebe3624cbf4dad3f58ba52ace808
jacobsanchezbejarano <jake_76847@hotmail.com>
2023-10-25 11:45:46 -0400

    test

--- scripts/script.js
+++ scripts/script.js
@@ -25,46 +25,45 @@ map.on('click', function(e) {
 
 // Handle form submission
 document.getElementById('submitBtn').addEventListener('click', function() {
-    // Gather form data
-    var formData = {
-        pedido_id: document.getElementById('pedido_id').value,
-        pedido_latitud: document.getElementById('pedido_latitud').value,
-        pedido_longitud: document.getElementById('pedido_longitud').value,
-        pedido_pais: document.getElementById('pedido_pais').value,
-        pedido_ciudad: document.getElementById('pedido_ciudad').value,
-        pedido_celular: document.getElementById('pedido_celular').value,
-    };
+        // ... (Tu código anterior)
 
-    // Send the data to your web service using AJAX or other methods.
-    fetch('https://envios-26fg.onrender.com/pedidos/', {
-        method: 'POST',
-        body: JSON.stringify(formData),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.success) {
-            // Show the success modal
-            $('#successModal').modal('show');
+        fetch('https://envios-26fg.onrender.com/pedidos/', {
+            method: 'POST',
+            body: JSON.stringify(formData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                // Show the success modal
+                document.getElementById('successModal').style.display = 'block';
 
-            // Clear the form
-            document.getElementById('pedido_id').value = '';
-            document.getElementById('pedido_latitud').value = '';
-            document.getElementById('pedido_longitud').value = '';
-            document.getElementById('pedido_pais').value = '';
-            document.getElementById('pedido_ciudad').value = '';
-            document.getElementById('pedido_celular').value = '';
-        } else {
-            // Show the error modal
-            $('#errorModal').modal('show');
-        }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        // Show the error modal in case of a network error
-        $('#errorModal').modal('show');
+                // Clear the form
+                document.getElementById('pedido_id').value = '';
+                document.getElementById('pedido_latitud').value = '';
+                document.getElementById('pedido_longitud').value = '';
+                document.getElementById('pedido_pais').value = '';
+                document.getElementById('pedido_ciudad').value = '';
+                document.getElementById('pedido_celular').value = '';
+            } else {
+                // Show the error modal
+                document.getElementById('errorModal').style.display = 'block';
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            // Show the error modal in case of a network error
+            document.getElementById('errorModal').style.display = 'block';
+        });
     });
-});
 
+    // Cerrar los modales cuando se haga clic en la "x"
+    document.getElementById('closeSuccessModal').addEventListener('click', function() {
+        document.getElementById('successModal').style.display = 'none';
+    });
+
+    document.getElementById('closeErrorModal').addEventListener('click', function() {
+        document.getElementById('errorModal').style.display = 'none';
+    });
