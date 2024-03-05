9e4bf442a348ef9c79cdb7460eeb90744c3ba803
jacobsanchezbejarano <jake_76847@hotmail.com>
2024-03-05 00:01:47 -0400

    updates

--- scripts/api_pedidos.js
+++ scripts/api_pedidos.js
@@ -34,3 +34,28 @@ function get_pedidos_pendientes(map) {
             console.error('Error:', error);
         });
 }
+
+async function enviarPedido(event) {
+    event.preventDefault(); // Previene el comportamiento predeterminado del formulario
+  
+    const formData = new FormData(document.getElementById("pedidoForm"));
+    const data = Object.fromEntries(formData.entries());
+  
+    try {
+      const response = await fetch('https://envios-26fg.onrender.com/pedidos', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
+  
+      if (!response.ok) {
+        throw new Error('Error al enviar los datos a la API.');
+      }
+  
+      console.log('Datos enviados exitosamente:', await response.json());
+    } catch (error) {
+      console.error('Error al enviar los datos a la API:', error);
+    }
+  }
