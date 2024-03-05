db4e0b10e444382ad4552251da6c61ec223dca81
jacobsanchezbejarano <jake_76847@hotmail.com>
2024-03-05 00:05:56 -0400

    updates

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,44 @@
+return;
 // Initialize the map
 var map = L.map('map').setView([-17.77871073951463, -63.183472859962734], 13);
+
+
+function get_pedidos_pendientes(map) {
+    var apiURL = 'https://envios-26fg.onrender.com/pedidos';
+
+    fetch(apiURL)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error en la solicitud a la API');
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Maneja los datos obtenidos de la API
+
+
+            data.forEach(ubicacion => {
+                // Crea un ícono personalizado para cada marcador
+                var customIcon = L.icon({
+                    iconUrl: 'https://cdn.icon-icons.com/icons2/1206/PNG/512/1491254387-pindestinationmaplocation_82942.png',
+                    iconSize: [32, 32],
+                    iconAnchor: [16, 32],
+                    popupAnchor: [0, -32]
+                });
+
+                // Crea y agrega el marcador al mapa con el ícono personalizado
+                var marker = L.marker([ubicacion.pedido_latitud, ubicacion.pedido_longitud], { icon: customIcon })
+                    .addTo(map)
+                    .bindPopup(ubicacion.nombre); // Muestra el nombre en el popup
+            });
+
+            // Itera sobre los datos y agrega marcadores al mapa
+            
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
 
 //LABEL AND ANCHOR
 //<a target="_blank" href="https://www.google.com/maps/place/'+str(lat)+','+str(lon)+'">Pedido: '+pedido+'</a>
