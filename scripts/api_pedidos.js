1a455886ece4a5d1514da59f4af392c258276502
jacobsanchezbejarano <jake_76847@hotmail.com>
2024-03-05 00:24:43 -0400

    update

--- scripts/api_pedidos.js
+++ scripts/api_pedidos.js
@@ -22,3 +22,49 @@ async function enviarPedido(event) {
       console.error('Error al enviar los datos a la API:', error);
     }
   }
+
+  async function obtenerPedidos() {
+    try {
+      const response = await fetch('https://envios-26fg.onrender.com/envios');
+      if (!response.ok) {
+        throw new Error('Error al obtener los pedidos.');
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error al obtener los pedidos:', error);
+      throw error;
+    }
+  }
+
+  
+  function dibujarTabla(pedidos) {
+    const tabla = document.createElement('table');
+    const encabezado = tabla.createTHead();
+    const filaEncabezado = encabezado.insertRow();
+    for (const key in pedidos[0]) {
+      const th = document.createElement('th');
+      th.textContent = key;
+      filaEncabezado.appendChild(th);
+    }
+    const cuerpo = tabla.createTBody();
+    pedidos.forEach(pedido => {
+      const fila = cuerpo.insertRow();
+      for (const key in pedido) {
+        const celda = fila.insertCell();
+        celda.textContent = pedido[key];
+      }
+    });
+    document.body.appendChild(tabla);
+  }
+  
+
+  (async () => {
+    try {
+      const pedidos = await obtenerPedidos();
+      dibujarTabla(pedidos);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  })();
+  
