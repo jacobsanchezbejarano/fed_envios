a7de9cba602404f05f2c07de92c7bd6370456bd1
jacobsanchezbejarano <jake_76847@hotmail.com>
2024-03-07 12:40:51 -0400

    update

--- scripts/api_pedidos.js
+++ scripts/api_pedidos.js
@@ -40,71 +40,90 @@ async function enviarPedido(event) {
     }
   }
 
+  function capitalize(str) {
+      return str.charAt(0).toUpperCase() + str.slice(1);
+  }
   
   function dibujarTabla(pedidos) {
-    const tabla = document.createElement('table');
-    const encabezado = tabla.createTHead();
+    const tabla = document.createElement('div');
+    tabla.classList.add('table-responsive');
+
+    const tablaInterna = document.createElement('table');
+    tablaInterna.classList.add('table', 'table-bordered', 'table-striped', 'table-hover', 'table-condensed', 'cf');
+
+    const encabezado = tablaInterna.createTHead();
     const filaEncabezado = encabezado.insertRow();
-    
+
     var header_pedidos = [
-      "Celular",
-      "Nombre",
-      "Cantidad",
-      "Estado Pedido",
-      "Tipo de Pedido",
-      "Hora entrega",
-      "Zona",
-      "Comentarios",
-      "Vendedor",
-      "Acciones"
+        {title: "Celular", class: "numeric"},
+        {title: "Nombre", class: ""},
+        {title: "Cantidad", class: "numeric"},
+        {title: "Estado Pedido", class: ""},
+        {title: "Tipo de Pedido", class: ""},
+        {title: "Hora entrega", class: "numeric"},
+        {title: "Zona", class: ""},
+        {title: "Comentarios", class: ""},
+        {title: "Vendedor", class: ""},
+        {title: "Acciones", class: ""}
     ];
-    for (const key of header_pedidos) {
+
+    for (const item of header_pedidos) {
       const th = document.createElement('th');
-      th.textContent = key;
+      th.textContent = item.title;
+      if (item.class) { // Verifica si item.class tiene un valor
+          th.classList.add(item.class);
+      }
       filaEncabezado.appendChild(th);
-    }
-    const cuerpo = tabla.createTBody();
-    pedidos.forEach(pedido => {
-      const fila = cuerpo.insertRow();
-      for (const key in pedido) {
-          switch (key) {
-              case 'pedido_celular':
-                  const celda = fila.insertCell();
-                  const enlace = document.createElement('a');
-                  enlace.href = 'https://wa.me/591' + pedido[key]; // Enlace de WhatsApp con el número de celular
-                  enlace.textContent = pedido[key];
-                  celda.appendChild(enlace);
-                  break;
-              case 'pedido_nombre':
-              case 'pedido_cantidad':
-              case 'pedido_estado':
-              case 'pedido_tipo':
-              case 'pedido_hora_entrega':
-              case 'pedido_zona':
-              case 'pedido_comentarios':
-              case 'pedido_vendedor':
-                  const celdaNormal = fila.insertCell();
-                  celdaNormal.textContent = pedido[key];
-                  break;
-              default:
-                  // No hacer nada o manejar otros casos si es necesario
-                  break;
-          }
-      }
-  
-      // Después del bucle, agregar el enlace al final si corresponde
-      if (pedido.pedido_latitud !== '') {
-          var link = document.createElement('a');
-          link.href = 'https://www.google.com/maps/place/' + pedido.pedido_latitud + ',' + pedido.pedido_longitud;
-          link.target = '_blank';
-          link.textContent = 'Ir a mapa';
-          const celda = fila.insertCell();
-          celda.appendChild(link);
-      }
-  });
-  
-    document.getElementById("lista_pedidos").appendChild(tabla);
   }
+
+    const cuerpo = tablaInterna.createTBody();
+    pedidos.forEach(pedido => {
+        const fila = cuerpo.insertRow();
+        for (const key in pedido) {
+            switch (key) {
+                case 'pedido_celular':
+                    const celda = fila.insertCell();
+                    const enlace = document.createElement('a');
+                    enlace.href = 'https://wa.me/591' + pedido[key]; // Enlace de WhatsApp con el número de celular
+                    enlace.textContent = pedido[key];
+                    celda.appendChild(enlace);
+                    celda.setAttribute('data-title', 'Celular');
+                    break;
+                case 'pedido_nombre':
+                case 'pedido_cantidad':
+                case 'pedido_estado':
+                case 'pedido_tipo':
+                case 'pedido_hora_entrega':
+                case 'pedido_zona':
+                case 'pedido_comentarios':
+                case 'pedido_vendedor':
+                    const celdaNormal = fila.insertCell();
+                    celdaNormal.textContent = pedido[key];
+                    celdaNormal.setAttribute('data-title', capitalize(key.replace('pedido_', '')));
+                    break;
+                default:
+                    // No hacer nada o manejar otros casos si es necesario
+                    break;
+            }
+        }
+
+        // Después del bucle, agregar el enlace al final si corresponde
+        if (pedido.pedido_latitud !== '') {
+            var link = document.createElement('a');
+            link.href = 'https://www.google.com/maps/place/' + pedido.pedido_latitud + ',' + pedido.pedido_longitud;
+            link.target = '_blank';
+            link.textContent = 'Ir a mapa';
+            const celda = fila.insertCell();
+            celda.appendChild(link);
+            celda.setAttribute('data-title', 'Acciones');
+        }
+    });
+
+    tabla.appendChild(tablaInterna);
+    document.getElementById("no-more-tables").appendChild(tabla);
+}
+
+
   
 
   (async () => {
