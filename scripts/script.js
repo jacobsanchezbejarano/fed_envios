dfa986dbb96b71527740191dfb17aa6a954113a9
jacobsanchezbejarano <jake_76847@hotmail.com>
2024-03-08 10:26:04 -0400

    updates

--- scripts/script.js
+++ scripts/script.js
@@ -29,7 +29,8 @@ function get_pedidos_pendientes(map) {
                 var marker = L.marker([ubicacion.pedido_latitud, ubicacion.pedido_longitud], { icon: customIcon })
                     .addTo(map)
                     .bindPopup('<a class="btn" target="_blank" href="https://www.google.com/maps/place/' + ubicacion.pedido_latitud + ',' + ubicacion.pedido_longitud + '"><button style="background-color:#007bff; color:white">Navegar en maps</button></a><br>Pedido: ' + ubicacion.pedido_nombre + ' <br>Hora: '+ ubicacion.pedido_hora_entrega + '<br>Cantidad: '+ ubicacion.pedido_cantidad + '<br>Estado: '+ ubicacion.pedido_estado + ''+
-                    '<br><a target="_blank" href="https://wa.me/591'+ ubicacion.pedido_celular+'"><button style="background-color:#25d366; color:#666">Celular: '+ ubicacion.pedido_celular+'</button></a>'
+                    '<br><a target="_blank" href="https://wa.me/591'+ ubicacion.pedido_celular+'"><button style="background-color:#25d366; color:#666">Celular: '+ ubicacion.pedido_celular+'</button></a>'+
+                    '<br><p>Comentarios: ' + (ubicacion.pedido_comentarios ? ubicacion.pedido_comentarios : 'Sin comentarios')+'</p>'
                     );
             });
 
